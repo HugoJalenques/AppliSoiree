5b12b585000e17935d62a8ae548d1e2db8aa44d2
Hugo Jalenques <Hugo Jalenques>
2016-03-27 23:35:43 -0400

    pull moi ca

--- Party2Nite/app/src/main/java/com/epita/rayanhugo/party2nite/MainActivity.java
+++ Party2Nite/app/src/main/java/com/epita/rayanhugo/party2nite/MainActivity.java
@@ -20,15 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -51,10 +42,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        Log.d("MainActivity", "lol");
     }
 
     public class Carte
